8050f2398a042f41034d4d2968eb22005aa5f23a
Zeeshan Motagaonwala <zeeshan.motagaonwala@oracle.com>
2024-03-11 16:25:33 +0530

    Adding config in connection for DB

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,38 @@
 // database/connection.js
-
 const mysql2 = require("mysql2");
+require('dotenv').config(); // Load environment variables from .env file
 
-const pool = mysql2.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "",
-    database: "e_commerce",
+let connectionParams;
+
+// Use flag to toggle between localhost and server configurations
+const useLocalhost = process.env.USE_LOCALHOST === 'true';
+
+if (useLocalhost) {
+    console.log("Inside local")
+    connectionParams = {
+        user: "root",
+        host: "localhost",
+        password: "",
+        database: "e_commerce",
+    };
+} else {
+    connectionParams = {
+        user: process.env.DB_SERVER_USER,
+        host: process.env.DB_SERVER_HOST,
+        password: process.env.DB_SERVER_PASSWORD,
+        database: process.env.DB_SERVER_DATABASE,
+    };
+}
+
+
+
+const pool = mysql2.createConnection(connectionParams);
+
+pool.connect((err) => {
+    if (err) console.log(err.message);
+    else console.log("DB Connection Done")
 });
 
 // Export the pool
 module.exports = pool;
+
